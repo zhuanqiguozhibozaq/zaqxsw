9cbf928fdf552ae827ff8b64b19f43449879b9f5
zhuanqiguozhibozaq <51935875+zhuanqiguozhibozaq@users.noreply.github.com>
2019-06-20 17:53:18 +0800

    Add files via upload

---
+++ 1170426011朱安琦2.py
@@ -0,0 +1,19 @@
+﻿import urllib.request
+url = 'http://www.baidu.com'
+proxy_handle = {
+    'http':'183.51.190.51:33913',
+    'http':'122.193.245.44:9999'
+}
+proxy = urllib.request.ProxyHandler(proxy_handle)
+opener = urllib.request.build_opener(proxy)
+request = urllib.request.Request(url=url)
+response = urllib.request.urlopen(request)
+print(response.read().decode('utf8'))
+while 1:
+    print(response.status)   
+    if (response.status != 200):
+        print('爬虫不成功')
+    break
+
+
+
