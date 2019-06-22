40249d3c76a960faeaf81fa51756dce132ae4238
zhuanqiguozhibozaq <51935875+zhuanqiguozhibozaq@users.noreply.github.com>
2019-06-22 08:47:38 +0800

    Add files via upload

--- 1170426011朱安琦2.py
+++ 1170426011朱安琦2.py
@@ -12,8 +12,35 @@ print(response.read().decode('utf8'))
 while 1:
     print(response.status)   
     if (response.status != 200):
-        print('爬虫不成功')
+        print('爬虫失败')
     break
+
+
+
+
+
+import urllib.request
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+url = 'http://www.baidu.com'
+request = urllib.request.Request(url,headers=headers)
+response = opener.open(request)
+for item in cookie:
+print(item.name,item.value)
+filename = 'tom.txt'
+cookie.save(filename=filename,ignore_discard=True,ignore_expires=True)
+cookie = http.cookiejar.MozillaCookieJar()
+cookie.load('tom.txt')
+print(cookie)
+##############################################
+try:
+    a = 100
+    b = 100
+    a + b
+except Exception as o:
+    print(o)
+else:
+    print('lalalala')
 
 
 
